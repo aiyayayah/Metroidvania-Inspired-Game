696bc26b742d7f0e911ca62bef5eebe913bc5f55
Kah Yong <cky734@gmail.com>
2025-04-16 11:24:28 +0800

    link idle and run anim

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -7,7 +7,9 @@ public class NewBehaviourScript : MonoBehaviour
 {
     public float speed = 1.0f;
     public float jumpForce = 10.0f;
+
     public Rigidbody2D playerRig;
+    public Animator playerAnim;
     void Start()
     {
         
@@ -19,9 +21,19 @@ public class NewBehaviourScript : MonoBehaviour
         float xInput = Input.GetAxisRaw("Horizontal");
         float yInput = Input.GetAxisRaw("Vertical");
         playerRig.velocity = new Vector2(xInput * speed, playerRig.velocity.y);
-       
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (xInput != 0) //pressed 
+        {
+            //playerAnim.Play("Run");
+            playerAnim.SetBool("isRunning", true);
+        }
+        else
+        {
+            //playerAnim.Play("Idle");
+            playerAnim.SetBool("isRunning", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             playerRig.velocity = new Vector2(playerRig.velocity.x, jumpForce);
         }
