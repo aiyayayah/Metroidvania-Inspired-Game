09c36f5fbe45783721592637aa401098a4252cf5
Kah Yong <cky734@gmail.com>
2025-04-23 17:10:23 +0800

    add player dead logic

--- Assets/Scripts/PlayerGetHit.cs
+++ Assets/Scripts/PlayerGetHit.cs
@@ -15,6 +15,7 @@ public class PlayerGetHit : MonoBehaviour
     public GameObject getHitPosition;
     public AudioClip getHitAudio;
     public float getHitTime = 0.5f;
+    public bool isDead = false;
 
     private void OnTriggerEnter2D(Collider2D collision) //Sent when another object enters a trigger collider attached to this object
     {
@@ -23,19 +24,42 @@ public class PlayerGetHit : MonoBehaviour
 
     public void GetHit(Collider2D collision)
     {
+        ControlHP();
         if (collision.tag == "EnemyAttack")
         {
-            playerAnim.SetTrigger("getHit");
-            conSound.PlayOneShot(getHitAudio, soundVolume);
-            playerClass.isAttack = true;
-            Invoke("EndGetHit", getHitTime);
-            Instantiate(getHitEffect, getHitPosition.transform.position, getHitPosition.transform.rotation);
+
 
         }
     }
-
     public void EndGetHit()
     {
         playerClass.isAttack = false;
     }
+
+    public void ControlHP()
+    {
+        playerClass.currentHP = playerClass.currentHP - 20;
+
+        if(playerClass.currentHP > 0)
+        {
+            playerAnim.SetTrigger("getHit");
+            conSound.PlayOneShot(getHitAudio, soundVolume);
+            playerClass.isAttack = true;
+            CancelInvoke("EndGetHit");
+            Invoke("EndGetHit", getHitTime);
+            Instantiate(getHitEffect, getHitPosition.transform.position, getHitPosition.transform.rotation);
+        }
+        else
+        {
+            if(isDead == false)
+            {
+                isDead = true;
+                CancelInvoke("EndGetHit");
+                playerClass.isAttack = true;
+                playerAnim.SetTrigger("Die");
+                Instantiate(getHitEffect, getHitPosition.transform.position, getHitPosition.transform.rotation);
+            }
+        }
+    }
+
 }
