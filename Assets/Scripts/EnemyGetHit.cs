6833331eb20b6c21da9c29782d08e471c5978705
Kah Yong <cky734@gmail.com>
2025-04-23 14:40:14 +0800

    update enemy getHit logic

--- Assets/Scripts/EnemyGetHit.cs
+++ Assets/Scripts/EnemyGetHit.cs
@@ -8,25 +8,32 @@ public class EnemyGetHit : MonoBehaviour
     public Animator enemyAnim;
     public AudioSource conSound;
     public float soundVolume = 0.5f;
+    public EnemyController enemyClass;
 
     [Header("Get Hit")]
     public GameObject getHitEffect;
     public GameObject getHitPosition;
     public AudioClip getHitAudio;
+    public float stunnedTimeAfterHitted = 1.0f;
 
     private void OnTriggerEnter2D(Collider2D collision) //Sent when another object enters a trigger collider attached to this object
     {
         GetHit(collision);
     }
-
     public void GetHit(Collider2D collision)
     {
         if (collision.tag == "PlayerAttack")
-        {
-            enemyAnim.SetTrigger("getHit");
+        { 
             conSound.PlayOneShot(getHitAudio, soundVolume);
+            enemyClass.enemyState = EnemyState.getHit;
+            CancelInvoke("InvokeGetHit");
+            Invoke("InvokeGetHit", stunnedTimeAfterHitted);
+            enemyAnim.SetTrigger("getHit");
             Instantiate(getHitEffect, getHitPosition.transform.position, getHitPosition.transform.rotation);
         }
     }
-
+    public void InvokeGetHit()
+    {
+        enemyClass.enemyState = EnemyState.moveToPlayer;
+    }
 }
