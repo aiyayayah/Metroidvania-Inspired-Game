a0b9acd88439fb30ca40ae0e9d68253eaf125a00
Kah Yong <cky734@gmail.com>
2025-04-24 00:31:36 +0800

    done slider music control

--- Assets/Scripts/soundVolume.cs
+++ Assets/Scripts/soundVolume.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI;
 
 public class soundVolume : MonoBehaviour
 {
-    public static float Audio = 1.0f;
-    public static float Music = 1.0f;
+    public static float Audio = 0.8f;
+    public static float Music = 0.5f;
+
     public Slider sliderAudio;
+    public Slider sliderMusic;
+    public AudioSource audioSourceMain;
 
     void Start()
     {
@@ -21,5 +24,10 @@ public class soundVolume : MonoBehaviour
     public void AudioChange()
     {
         Audio = sliderAudio.value;
+    }
+    public void MusicChange()
+    {
+        Music = sliderMusic.value;
+        audioSourceMain.volume = Music;
     }
 }
